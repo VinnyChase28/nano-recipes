dcf2e86316eb22796abd7676ad6ccc1f3e990a10
Vincent <vincentgauthier068@gmail.com>
2022-05-05 18:38:33 -0700

    change name to nanorecipes

--- application/utils/FacebookConfig.js
+++ application/utils/FacebookConfig.js
@@ -1,6 +1,6 @@
 export default {
   config: {
-    application_name: "Yonia",
+    application_name: "Nanorecipes",
     application_id: "1018048932321041",
     permissions: ["public_profile", "email"],
   },
