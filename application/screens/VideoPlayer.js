3918f447dc0ca94796040fff79ae1e3037069ca9
Vincent <vincentgauthier068@gmail.com>
2022-05-20 01:38:48 -0700

    remove expo

--- application/screens/VideoPlayer.js
+++ application/screens/VideoPlayer.js
@@ -1,39 +1,45 @@
-import React, {Component} from 'react';
-import AppPreLoader from '../components/AppPreLoader';
-import {Dimensions, StatusBar, Platform, View, TouchableOpacity} from 'react-native';
-import ConfigApp from '../utils/ConfigApp';
-import { WebView } from 'react-native-webview';
-import { Container, Text, Button} from 'native-base';
-import Icon from 'react-native-vector-icons/SimpleLineIcons';
+import React, { Component } from "react";
+import AppPreLoader from "../components/AppPreLoader";
+import {
+  Dimensions,
+  StatusBar,
+  Platform,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import ConfigApp from "../utils/ConfigApp";
+import { WebView } from "react-native-webview";
+import { Container, Text, Button } from "native-base";
+import Icon from "react-native-vector-icons/SimpleLineIcons";
 
-var styles = require('../../assets/files/Styles');
-var {height, width} = Dimensions.get('window');
+var styles = require("../../assets/files/Styles");
+var { height, width } = Dimensions.get("window");
 
 export default class VideoP extends Component {
-
   render() {
-
-  var UrlVideo = this.props.route.params.VideoLINK;
+    var UrlVideo = this.props.route.params.VideoLINK;
 
     return (
-
-<Container style={{backgroundColor: '#000000'}}>
-
-<TouchableOpacity onPress={() => this.props.navigation.goBack()} style={{position: 'absolute', right: 15, top: 40,zIndex: 9999}}>
-          <View style={{width: 30, height: 50}}>
-            <Icon name='close' style={{color: '#fff', fontSize: 24}} />
+      <Container style={{ backgroundColor: "#000000" }}>
+        <TouchableOpacity
+          onPress={() => this.props.navigation.goBack()}
+          style={{ position: "absolute", right: 15, top: 40, zIndex: 9999 }}
+        >
+          <View style={{ width: 30, height: 50 }}>
+            <Icon name="close" style={{ color: "#fff", fontSize: 24 }} />
           </View>
-</TouchableOpacity>
+        </TouchableOpacity>
 
-<WebView
-source={{ uri: 'https://www.youtube.com/embed/'+UrlVideo+'?rel=0&autoplay=0&showinfo=0&controls=0&playsinline=1&modestbranding=1'}}
-style={{ width: width, height: height * 0.30}}
-/>
-
-</Container>
-
-   );
-
+        <WebView
+          source={{
+            uri:
+              "https://www.youtube.com/embed/" +
+              UrlVideo +
+              "?rel=0&autoplay=0&showinfo=0&controls=0&playsinline=1&modestbranding=1",
+          }}
+          style={{ width: width, height: height * 0.3 }}
+        />
+      </Container>
+    );
   }
-
 }
